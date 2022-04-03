2c43619144acea96086b72b347a53d391cd921b9
KamilB00 <bonkowski.kamil@icloud.com>
2022-04-03 23:17:51 +0200

    some changes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 #include <cstdlib>
 #include <iostream>
 
-using std::cout;
-using std::endl;
-using std::cin;
-
 int random(int min, int max) {
     static bool first = true;
     if (first) {
@@ -16,47 +12,46 @@ int random(int min, int max) {
     return min + rand() % ((max + 1) - min);
 }
 
-void randomSleepTime() {
-    int sleepTime = random(1, 10);
-    cout << "Sleeping for: " << sleepTime << " s" << endl;
-    sleep(sleepTime);
-}
-
 int fibonacci(int n) {
-   if (n <=1)
-       return n;
-    return fibonacci(n-1) + fibonacci(n-2);
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-void counting() {
-    cout << "Calculating..." << endl;
-    int randElement = random(4,20);
-    int result =  fibonacci(randElement);
-    cout<<"Fibonacci sequence "<<randElement<<"-th element is: "<<result<<endl;
+int counting(int n) {
+    int result = fibonacci(n);
+    return result;
 }
 
 void *threadFunction(void *arg) {
-    cout << "Thread created" << endl;
-    counting();
-    randomSleepTime();
-    cout << "Ending thread" << endl;
-    cout << endl;
+    int index = *((int *) arg);
+    int randElement = random(4, 20);
+    int sleepTime = random(1, 10);
+    printf("Thread %d: Created\n", index);
+    printf("Thread %d: Result of calculating %d-th element of Fibonacci sequence is: %d\n", index, randElement,
+           counting(randElement));
+    printf("Thread %d: Sleep for:  %d s\n", index, sleepTime);
+    sleep(sleepTime);
+    printf("Thread %d: Ending\n", index);
     return nullptr;
 }
 
-void createThreads(int n) {
-    for (int i = 0; i < n; ++i) {
-        pthread_t newThread;
-        cout << "Creating Thread " << i + 1 << endl;
-        pthread_create(&newThread, nullptr, threadFunction, nullptr);
-        pthread_join(newThread, nullptr);
+void createThreads(int numberOfThreads) {
+    pthread_t newThread[numberOfThreads];
+    int threadArguments[numberOfThreads];
+    for (int i = 1; i <= numberOfThreads; ++i) {
+        threadArguments[i] = i;
+        pthread_create(&newThread[i], nullptr, threadFunction, &threadArguments[i]);
+    }
+    for (int i = 1; i <= numberOfThreads; ++i) {
+        pthread_join(newThread[i], nullptr);
     }
 }
 
 int numberOfThreads() {
     int number_of_threads;
-    cout << "How many threads would you like to create: ";
-    cin >> number_of_threads;
+    printf("How many threads would you like to create: ");
+    scanf("%d", &number_of_threads);
     return number_of_threads;
 }
 
